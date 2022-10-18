0514761cc6eac910c9386908206c787b45969c74
ksana stankevich <ksanastankevich@gmail.com>
2022-10-18 19:07:55 +0300

    Добавлена еще одна задача на объекты

--- task11.js
+++ task11.js
@@ -36,9 +36,20 @@ for (let key in salaries) {
 }
 alert(sum);
 
-for (const key in object) {
-    if (Object.hasOwnProperty.call(object, key)) {
-        const element = object[key];
-        
+// Создайте функцию multiplyNumeric(obj), которая умножает все числовые свойства объекта obj на 2.
+
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  };
+  
+function multiplyNumeric(obj) {
+    for (const key in obj) {
+        if (typeof obj[key] == 'number') {
+            obj[key] *= 2;
+        }
     }
 }
+
+multiplyNumeric(menu);
